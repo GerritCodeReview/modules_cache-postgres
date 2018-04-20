61c42b2ac85633aefea1789656ef005e5368f740
Jacek Centkowski <jcentkowski@collab.net>
2018-04-20 11:28:30 +0200

    Remove bloomfilter from postgres implementation
    It seems to be solving the problems that real database engine
    like postgres don't have. It also adds this front loading step
    that reaches for all data from cache.
    
    Change-Id: I51271eeed1893947e1d7f8ec2ab1d209154b0f5c
    Signed-off-by: Jacek Centkowski <jcentkowski@collab.net>

--- src/main/java/com/googlesource/gerrit/modules/cache/pg/PgCacheImpl.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/pg/PgCacheImpl.java
@@ -79,12 +79,10 @@ public class PgCacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
       return h.value;
     }
 
-    if (store.mightContain(key)) {
-      h = store.getIfPresent(key);
-      if (h != null) {
-        mem.put(key, h);
-        return h.value;
-      }
+    h = store.getIfPresent(key);
+    if (h != null) {
+      mem.put(key, h);
+      return h.value;
     }
     return null;
   }
@@ -94,16 +92,14 @@ public class PgCacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
     return mem.get(
             key,
             () -> {
-              if (store.mightContain(key)) {
-                ValueHolder<V> h = store.getIfPresent(key);
-                if (h != null) {
-                  return h;
-                }
+              ValueHolder<V> h = store.getIfPresent(key);
+              if (h != null) {
+                return h;
               }
 
-              ValueHolder<V> h = new ValueHolder<>(valueLoader.call());
-              h.created = TimeUtil.nowMs();
-              executor.execute(() -> store.put(key, h));
+              ValueHolder<V> newHolder = new ValueHolder<>(valueLoader.call());
+              newHolder.created = TimeUtil.nowMs();
+              executor.execute(() -> store.put(key, newHolder));
               return h;
             })
         .value;
@@ -120,7 +116,7 @@ public class PgCacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
   @SuppressWarnings("unchecked")
   @Override
   public void invalidate(Object key) {
-    if (keyType.getRawType().isInstance(key) && store.mightContain((K) key)) {
+    if (keyType.getRawType().isInstance(key)) {
       executor.execute(() -> store.invalidate((K) key));
     }
     mem.invalidate(key);
@@ -145,10 +141,6 @@ public class PgCacheImpl<K, V> extends AbstractLoadingCache<K, V> implements Per
   @Override
   public DiskStats diskStats() {
     return store.diskStats();
-  }
-
-  void start() {
-    store.open();
   }
 
   void stop() {
